763dc888384f03e0e874e93ef2df8b361eed47d3
rexim <reximkut@gmail.com>
2018-01-02 00:06:39 +0700

    Finish the implementation

--- main.fs
+++ main.fs
@@ -3,19 +3,41 @@ open System
 type Cell = Dead | Alive
 type World = Cell list list
 
+let parseCell (c: char): Cell =
+  match c with
+  | '.' -> Dead
+  | '@' -> Alive
+
+let makeWorld (map: string list): World =
+  [for row in map do
+     yield [for c in row do
+              yield (parseCell c)]]
+
 let renderCell (cell: Cell): string =
   match cell with
   | Dead -> "."
   | Alive -> "@"
 
 let countNeighbors (world: World) ((i, j) : int * int): int =
-  0
+  [for i in -1 .. 1 do
+   for j in -1 .. 1 do
+   yield (i, j)]
+  |> List.filter (fun x -> x <> (0, 0))
+  |> List.map (fun (di, dj) ->
+         try
+           match world.[i + di].[j + dj] with
+           | Dead -> 0
+           | Alive -> 1
+         with
+         | :? System.ArgumentException as ex -> 0)
+  |> List.fold (+) 0
 
 let nextGenCell (world: World) ((i, j): int * int): Cell =
   let neighbors = countNeighbors world (i, j) in
   match world.[i].[j] with
   | Dead when neighbors = 3 -> Alive
-  | Alive -> Alive
+  | Alive when neighbors < 2 || neighbors > 3 -> Dead
+  | x -> x
 
 let updateWorld (world: World): World =
   world
@@ -39,7 +61,14 @@ let rec gameLoop (world: World): int =
 
 [<EntryPoint>]
 let main _ =
-  let n = 10
-  let m = 10
-  let world = [ for i in 1 .. n -> [ for j in 1 .. n -> Dead ] ]
+  let world = makeWorld [".@........";
+                         "..@.......";
+                         "@@@.......";
+                         "..........";
+                         "..........";
+                         "..........";
+                         "..........";
+                         "..........";
+                         "..........";
+                         ".........."]
   gameLoop world
