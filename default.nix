e247f88fda6bb5c51da4caced77964c3d7988bce
rexim <reximkut@gmail.com>
2018-01-05 23:48:45 +0700

    Setup MonoDevelop solution and stuff (#1)

--- default.nix
+++ default.nix
@@ -1,11 +1,13 @@
 with import <nixpkgs> {}; rec {
+  frameworkVersion = "4.5";
   fsharpVersion = "4.0";
   assemblyVersion = "4.4.0.0";
 
   fsharpProbeEnv = stdenv.mkDerivation {
       name = "FsharpProbe";
-      buildInputs = [ mesa mono46 dotnetPackages.Nuget xorg.libX11 fsharp ];
+      buildInputs = [ mesa mono dotnetPackages.Nuget xorg.libX11 fsharp ];
       LD_LIBRARY_PATH="${xorg.libX11}/lib:${mesa}/lib";
-      MONO_PATH="${fsharp}/lib/mono/Reference Assemblies/Microsoft/FSharp/.NETFramework/v${fsharpVersion}/${assemblyVersion}/";
+      FSharpTargetsPath="${fsharp}/lib/mono/${frameworkVersion}/Microsoft.FSharp.Targets";
+      MONO_PATH="${fsharp}/lib/mono/Reference\ Assemblies/Microsoft/FSharp/.NETFramework/v${fsharpVersion}/${assemblyVersion}/";
   };
 }
